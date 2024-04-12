8cfc87e38365f8c75bca3377e2b4403a9e2055aa
xRaphae1 <166812813+xRaphae1@users.noreply.github.com>
2024-04-12 21:21:25 +0700

    Update xRaphael_Loader.lua

--- xRaphael_Loader.lua
+++ xRaphael_Loader.lua
@@ -766,12 +766,18 @@ local ExamList = {}
 for i = 1, 20 do
     table.insert(ExamList, "Option "..i)
 end
-local CombatFramework = require(game:GetService("Players").LocalPlayer.PlayerScripts:WaitForChild("CombatFramework"))
-local CombatFrameworkR = getupvalues(CombatFramework)[2]
-local RigController = require(game:GetService("Players")["LocalPlayer"].PlayerScripts.CombatFramework.RigController)
-local RigControllerR = getupvalues(RigController)[2]
-local realbhit = require(game.ReplicatedStorage.CombatFramework.RigLib)
-local cooldownfastattack = tick()
+local CombatFramework = require(game:GetService("Players").LocalPlayer.PlayerScripts.CombatFramework)
+local Camera = require(game.ReplicatedStorage.Util.CameraShaker)
+Camera:Stop()
+coroutine.wrap(function()
+    game:GetService("RunService").Stepped:Connect(function()
+        if getupvalues(CombatFramework)[2]['activeController'].timeToNextAttack then
+            getupvalues(CombatFramework)[2]['activeController'].timeToNextAttack = 0
+            getupvalues(CombatFramework)[2]['activeController'].hitboxMagnitude = 25
+            getupvalues(CombatFramework)[2]['activeController']:attack()
+        end
+    end)
+end)()
 
 function CurrentWeapon()
 	local ac = CombatFrameworkR.activeController
