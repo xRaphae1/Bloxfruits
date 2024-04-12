f3b3a9019a4b412d41fe1564f21a1abf6f168614
xRaphae1 <166812813+xRaphae1@users.noreply.github.com>
2024-04-12 22:01:12 +0700

    Update xRaphael_Loader.lua

--- xRaphael_Loader.lua
+++ xRaphael_Loader.lua
@@ -766,6 +766,12 @@ local ExamList = {}
 for i = 1, 20 do
     table.insert(ExamList, "Option "..i)
 end
+local CombatFramework = require(game:GetService("Players").LocalPlayer.PlayerScripts:WaitForChild("CombatFramework"))
+local CombatFrameworkR = getupvalues(CombatFramework)[2]
+local RigController = require(game:GetService("Players")["LocalPlayer"].PlayerScripts.CombatFramework.RigController)
+local RigControllerR = getupvalues(RigController)[2]
+local realbhit = require(game.ReplicatedStorage.CombatFramework.RigLib)
+local cooldownfastattack = tick()
 
 function CurrentWeapon()
 	local ac = CombatFrameworkR.activeController
